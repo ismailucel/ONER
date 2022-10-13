c8b363303e3fafb3bedc3a3c59866c91baf66376
İsmail Üçel <ismailucel@gmail.com>
2022-10-13 09:49:34 +0300

    Add project files.

---
+++ SuggestionAppUI/RegisterServices.cs
@@ -0,0 +1,23 @@
+﻿namespace SuggestionAppUI;
+
+public static class RegisterServices
+{
+   // Extension method for extending the dependency injection services
+   public static void ConfigureServices(this WebApplicationBuilder builder)
+   {
+
+      // Add services to the container.
+      builder.Services.AddRazorPages();
+      builder.Services.AddServerSideBlazor();
+      builder.Services.AddMemoryCache();
+
+     
+
+      // Add Dependency Injection container for DataAccess classes
+      builder.Services.AddSingleton<IDbConnection, DbConnection>(); // creates a single instance for everyone
+      builder.Services.AddSingleton<ICategoryData, MongoCategoryData>();
+      builder.Services.AddSingleton<IStatusData, MongoStatusData>();
+      builder.Services.AddSingleton<IUserData, MongoUserData>();
+      builder.Services.AddSingleton<ISuggestionData, MongoSuggestionData>();
+   }
+}
